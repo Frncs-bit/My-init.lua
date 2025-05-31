c9fc0c2e77310e60d9e443b7a3776b2a367b0ab6
Frncs-bit <franciscoiramsampaioufc@gmail.com>
2025-05-30 22:22:28 -0300

    Update init.lua
    transparencia no terminal, seguido de alteração no kitty

--- init.lua
+++ init.lua
@@ -54,7 +54,7 @@ require("lazy").setup({
         options = {
           icons_enabled = true,
           theme = 'auto', -- Ou 'catppuccin', 'tokyonight'
-          component_separators = { '', '' },
+         component_separators = { '', '' },
           section_separators = { '', '' },
           disabled_filetypes = { 'NvimTree', 'packer', 'alpha', 'lazy' },
           always_divide_middle = true,
@@ -324,8 +324,62 @@ require("lazy").setup({
 --             TEMA
 -- =============================================
 
--- vim.cmd.colorscheme("catppuccin")
-vim.cmd([[colorscheme tokyodark]]) -- Deixei comentado caso queira usar tokyodark
+ vim.cmd.colorscheme("catppuccin")
+-- vim.cmd([[colorscheme tokyodark]]) -- Deixei comentado caso queira usar tokyodark
+require("catppuccin").setup({
+    flavour = "macchiato", -- latte, frappe, macchiato, mocha
+    background = { -- :h background
+        light = "latte",
+        dark = "mocha",
+    },
+    transparent_background = true, -- disables setting the background color.
+    show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+    term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+    dim_inactive = {
+        enabled = false, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+    },
+    no_italic = false, -- Force no italic
+    no_bold = false, -- Force no bold
+    no_underline = false, -- Force no underline
+    styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" }, -- Change the style of comments
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+        -- miscs = {}, -- Uncomment to turn off hard-coded styles
+    },
+    color_overrides = {},
+    custom_highlights = {},
+    default_integrations = true,
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+            enabled = true,
+            indentscope_color = "",
+        },
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+    },
+})
+
+-- setup must be called before loading
+vim.cmd.colorscheme "catppuccin"
+
+
+-- setup must be called before loading
 
 -- =============================================
 --         C++ CODE RUNNER SIMPLES
@@ -384,4 +438,5 @@ vim.api.nvim_set_keymap('t', '<Esc><Esc>', '<C-\\><C-n>', { noremap = true, sile
 -- ===========================================================================
 -- Fim do init.lua
 -- ===========================================================================
+
 
